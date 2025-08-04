c575db5b2691376707fa4842122d3e745d46b715
tinkywinsky <winandanurf@gmail.com>
2025-08-04 23:45:57 +0700

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ def get_connection():
     """Membuat koneksi ke database MySQL."""
     try:
         conn = mysql.connector.connect(
-            host='localhost',        # Ganti dengan host MySQL Anda
+            host='safir.hidden-server.net',        # Ganti dengan host MySQL Anda
             database='tugasakh_login_app',  # Ganti dengan nama database Anda
             user='tugasakh_winan',             # Ganti dengan username MySQL Anda
             password='winandamendes'   # Ganti dengan password MySQL Anda
@@ -21,7 +21,7 @@ def get_connection():
 def get_sqlalchemy_engine():
     user = 'tugasakh_winan'
     password = 'windandamendes'
-    host = 'localhost'
+    host = 'safir.hidden-server.net'
     port = 3306
     database = 'tugasakh_login_app'
 
@@ -72,6 +72,7 @@ def delete_data(id):
     cursor.execute(query, (id,))
     conn.commit()
     conn.close()
+
 
 
 
