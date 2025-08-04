eb972d9b04ea00ef8cef6d0d6c1af5f2e898e0b3
tinkywinsky <winandanurf@gmail.com>
2025-08-04 20:49:19 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,12 +5,13 @@ def create_connection():
     """Fungsi untuk membuat koneksi ke database MySQL."""
     try:
         conn = mysql.connector.connect(
-            host="localhost",        # Ganti dengan host database Anda
-            user="root",             # Ganti dengan username MySQL Anda
-            password="",  # Ganti dengan password MySQL Anda
-            database="login_app"     # Ganti dengan nama database Anda
+            host=st.secrets["DB_HOST"],
+            database=st.secrets["DB_DATABASE"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"]    # Ganti dengan nama database Anda
         )
         return conn
     except mysql.connector.Error as e:
         st.error(f"Gagal terhubung ke database: {e}")
         return None
+
