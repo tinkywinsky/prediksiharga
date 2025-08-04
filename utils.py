790321e8a4912e54e4e45dfaab4b07012284b4d4
tinkywinsky <winandanurf@gmail.com>
2025-08-04 23:52:56 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,15 +3,11 @@ import streamlit as st
 
 def create_connection():
     """Fungsi untuk membuat koneksi ke database MySQL."""
-    try:
-        conn = mysql.connector.connect(
+        return mysql.connector.connect(
             host=st.secrets["DB_HOST"],
             database=st.secrets["DB_DATABASE"],
             user=st.secrets["DB_USER"],
             password=st.secrets["DB_PASSWORD"]    # Ganti dengan nama database Anda
         )
-        return conn
-    except mysql.connector.Error as e:
-        st.error(f"Gagal terhubung ke database: {e}")
-        return None
+
 
