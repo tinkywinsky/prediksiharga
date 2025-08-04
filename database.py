8a5766c8671a1c22dd7934e61ce2de7801597044
tinkywinsky <winandanurf@gmail.com>
2025-08-05 00:58:07 +0700

    Update database.py

--- database.py
+++ database.py
@@ -8,11 +8,11 @@ def get_connection():
     """Membuat koneksi ke database MySQL menggunakan Streamlit Secrets."""
     try:
         conn = mysql.connector.connect(
-           user = st.secrets["DB_USER"]
-    password = st.secrets["DB_PASSWORD"]
-    host = st.secrets["DB_HOST"]
-    port = 3306  # Port default untuk MySQL
-    database = st.secrets["DB_DATABASE"] # Password dari Streamlit Secrets
+            user = st.secrets["DB_USER"]
+            password = st.secrets["DB_PASSWORD"]
+        host = st.secrets["DB_HOST"]
+        port = 3306  # Port default untuk MySQL
+        database = st.secrets["DB_DATABASE"] # Password dari Streamlit Secrets
         )
         if conn.is_connected():
             print("Koneksi berhasil ke database.")
@@ -82,6 +82,7 @@ def delete_data(id):
         cursor.execute(query, (id,))
         conn.commit()
         conn.close()
+
 
 
 
